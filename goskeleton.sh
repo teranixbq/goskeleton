827f0ef6397817e0a8a34f165e412b740e7cb017
Hanief Fathul B.A <hanieffathulb03@gmail.com>
2024-02-27 17:06:44 +0000

    feat : testing choice

--- goskeleton.sh
+++ goskeleton.sh
@@ -4,3 +4,20 @@ echo -n "Folder Name: "
 read folder_name
 
 mkdir "$folder_name"
+
+echo "Pilih pattern:"
+echo "1. Default"
+
+read -p "Pilihan Anda: " choice
+
+case $choice in
+    1)
+        default() {
+            git clone --depth=1 $(curl -s https://api.github.com/repos/teranixbq/goskeleton/contents/default | grep -o 'git://[^"]*' | head -n 1) "$directory/$folder_name"
+        }
+        default
+        ;;
+    *)
+        echo "Pilihan tidak valid"
+        ;;
+esac
